4989d7aa2e038e9d53254be7398965dca480fbe4
Hussam812 <hossamhosnihussien@gmail.com>
2023-11-08 08:17:42 +0200

    navbar animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HiMenuAlt1, HiX } from "react-icons/hi";
 import { navLinks } from "../../data";
 import NavLink from "./NavLink";
 import MobileNav from "./MobileNav";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState(null);
+
+  useEffect(() => {
+    const scrollActive = () => {
+      setActive(window.scrollY > 20);
+    };
+    window.addEventListener("scroll", scrollActive);
+    return () => window.removeEventListener("scroll", scrollActive);
+  }, [active]);
   return (
-    <div className="fixed w-full top-0 left-0 z-20 ">
+    <div
+      className={`${
+        active ? "shadow-lg bg-Solitude" : ""
+      } fixed w-full top-0 left-0 z-20  `}
+    >
       <div>
-        <div className="container py-4 mx-auto flex items-center justify-between px-2">
+        <div
+          className={`${
+            active ? "py-2 transition-all duration-300" : " py-4"
+          } container mx-auto flex items-center justify-between px-2`}
+        >
           <div className="flex items-center  gap-4 ">
             <HiMenuAlt1
               className="text-3xl sm:hidden cursor-pointer"
